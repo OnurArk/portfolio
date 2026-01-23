d5a901147de8569efec676e8ef993831f85c468b
OnurArk <onurark11@gmail.com>
2026-01-23 16:06:15 +0300

    refactor: update Links component to include unique IDs for links and improve layout styling

--- src/components/dashboard/Links.tsx
+++ src/components/dashboard/Links.tsx
@@ -6,31 +6,30 @@ import { useThemeSettings } from "@/contexts/ThemeSettingsContext";
 
 const links = [
   {
+    id: 1,
     icon: "mdi:github",
     href: "https://github.com/OnurArk",
   },
   {
+    id: 2,
     icon: "mdi:linkedin",
     href: "https://www.linkedin.com/in/mehmet-onur-arik-5b589b249/",
   },
   {
-    icon: "solar:link-line-duotone",
-    href: "https://www.linkedin.com/in/your-profile",
-  },
-  {
-    icon: "solar:link-line-duotone",
-    href: "https://www.linkedin.com/in/your-profile",
-  },
+    id: 3,
+    icon: "bx:mail-send",
+    href: "mailto:onurark11@gmail.com"
+  }
 ];
 
 export function Links() {
   const { mode } = useThemeSettings();
 
   return (
-    <Box sx={{display: "flex", justifyContent:"center", alignItems:"center"}}>
+    <Box sx={{position: "relative", display: "flex", justifyContent:"center", alignItems:"center"}}>
     <Stack direction="row" spacing={2} sx={{justifyContent:"center", alignItems:"center"}}>
      {links.map((link) => (
-      <Card key={link.icon} sx={{width: "fit-content", p: 1.2 , cursor: "pointer"}} onClick={() => window.open(link.href, "_blank")}>
+      <Card key={link.id} sx={{width: "fit-content", p: 1.2 , cursor: "pointer"}} onClick={() => window.open(link.href, "_blank")}>
         <Icon icon={link.icon} width={36} height={36} />
       </Card>
      ))}
