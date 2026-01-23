9ff056ed5a20bf5d7119b9cc91b518c069615b2f
OnurArk <onurark11@gmail.com>
2026-01-23 14:53:47 +0300

    refactor: Links component to dynamically render social media links

--- src/components/dashboard/Links.tsx
+++ src/components/dashboard/Links.tsx
@@ -4,24 +4,36 @@ import { Icon } from "@iconify/react";
 import { Box, Stack, Card } from "@mui/material";
 import { useThemeSettings } from "@/contexts/ThemeSettingsContext";
 
+const links = [
+  {
+    icon: "mdi:github",
+    href: "https://github.com/OnurArk",
+  },
+  {
+    icon: "mdi:linkedin",
+    href: "https://www.linkedin.com/in/mehmet-onur-arik-5b589b249/",
+  },
+  {
+    icon: "solar:link-line-duotone",
+    href: "https://www.linkedin.com/in/your-profile",
+  },
+  {
+    icon: "solar:link-line-duotone",
+    href: "https://www.linkedin.com/in/your-profile",
+  },
+];
+
 export function Links() {
   const { mode } = useThemeSettings();
 
   return (
     <Box sx={{display: "flex", justifyContent:"center", alignItems:"center"}}>
     <Stack direction="row" spacing={2} sx={{justifyContent:"center", alignItems:"center"}}>
-    <Card sx={{width: "fit-content", p: 2 }}>
-             <Icon icon="solar:link-line-duotone" width={24} height={24} />
-       </Card>
-    <Card sx={{width: "fit-content", p: 2 }}>
-             <Icon icon="solar:link-line-duotone" width={24} height={24} />
+     {links.map((link) => (
+      <Card key={link.icon} sx={{width: "fit-content", p: 1.2 , cursor: "pointer"}} onClick={() => window.open(link.href, "_blank")}>
+        <Icon icon={link.icon} width={36} height={36} />
       </Card>
-    <Card sx={{width: "fit-content", p: 2 }}>
-           <Icon icon="solar:link-line-duotone" width={24} height={24} />
-      </Card>
-      <Card sx={{width: "fit-content", p: 2 }}>
-           <Icon icon="solar:link-line-duotone" width={24} height={24} />
-      </Card>
+     ))}
     </Stack>
     </Box>
   );
